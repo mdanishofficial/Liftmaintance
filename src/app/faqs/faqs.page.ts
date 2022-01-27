b88009fc533826d1ebe71f6985880dbccb93cf2e
Huzaifa Sarmad <mdanishofficial786@gmail.com>
2022-01-27 17:49:36 +0500

    commit

--- src/app/faqs/faqs.page.ts
+++ src/app/faqs/faqs.page.ts
@@ -31,14 +31,14 @@ export class FaqsPage implements OnInit {
   }
   faqs_data = []
 
-  selected = []
-  changeselected(id) {
-    for (var i = 0; i < this.faqs_data.length; i++) {
-      this.selected[i] = false
-    }
-    this.selected[id] = true
-    console.log(this.selected)
-  }
+  // selected = []
+  // changeselected(id) {
+  //   for (var i = 0; i < this.faqs_data.length; i++) {
+  //     this.selected[i] = false
+  //   }
+  //   this.selected[id] = true
+  //   console.log(this.selected)
+  // }
   notifications() {
     this.router.navigateByUrl('tabs/notifications');
   }
@@ -48,83 +48,4 @@ export class FaqsPage implements OnInit {
   async faqs() {
     this.router.navigateByUrl('/tabs/faqs');
   }
-  panels = [
-
-    {
-      active: true,
-      disabled: false,
-      name: 'Donec vel magna et nulla porta laoreet.',
-      description: 'Donec vel magna et nulla porta laoreet. Pellentesque sed tortor ex. Ut luctus sodales ante, quis aliquam libero efficitur sit amet. In sodales pellentesque venenatis. Cras mattis suscipit diam, et aliquet metus vehicula non. Ut at rutrum nunc. Fusce lobortis, turpis ac interdum blandit, purus dui blandit justo',
-      customStyle: {
-        background: '#FCFCFC',
-        'border-radius': '4px',
-        'margin-bottom': '24px',
-        border: '0px',
-        'margin-top': '-2em',
-        'font-size': '16px',
-        color: '#6E6E6E',
-      }
-    },
-    {
-      active: false,
-      disabled: true,
-      name: 'Ut luctus sodales ante, quis aliquam',
-      icon: 'right',
-      description: 'Donec vel magna et nulla porta laoreet. Pellentesque sed tortor ex. Ut luctus sodales ante, quis aliquam libero efficitur sit amet. In sodales pellentesque venenatis. Cras mattis suscipit diam, et aliquet metus vehicula non. Ut at rutrum nunc. Fusce lobortis, turpis ac interdum blandit, purus dui blandit justo',
-      customStyle: {
-        background: '#FCFCFC',
-        'border-radius': '4px',
-        'margin-bottom': '24px',
-        border: '0px',
-        'margin-top': '-2em',
-        'font-size': '16px',
-      }
-    },
-    {
-      active: false,
-      disabled: true,
-      name: 'nibh erat convallis nisl',
-      icon: 'right',
-      description: 'Donec vel magna et nulla porta laoreet. Pellentesque sed tortor ex. Ut luctus sodales ante, quis aliquam libero efficitur sit amet. In sodales pellentesque venenatis. Cras mattis suscipit diam, et aliquet metus vehicula non. Ut at rutrum nunc. Fusce lobortis, turpis ac interdum blandit, purus dui blandit justo',
-      customStyle: {
-        background: '#FCFCFC',
-        'border-radius': '4px',
-        'margin-bottom': '24px',
-        border: '0px',
-        'margin-top': '-2em',
-        'font-size': '16px',
-      }
-    },
-    {
-      active: false,
-      disabled: true,
-      name: 'Duis justo nisi, suscipit vel quam eleifend, ',
-      icon: 'right',
-      description: 'Donec vel magna et nulla porta laoreet. Pellentesque sed tortor ex. Ut luctus sodales ante, quis aliquam libero efficitur sit amet. In sodales pellentesque venenatis. Cras mattis suscipit diam, et aliquet metus vehicula non. Ut at rutrum nunc. Fusce lobortis, turpis ac interdum blandit, purus dui blandit justo',
-      customStyle: {
-        background: '#FCFCFC',
-        'border-radius': '4px',
-        'margin-bottom': '24px',
-        border: '0px',
-        'margin-top': '-2em',
-        'font-size': '16px',
-      }
-    },
-    {
-      active: false,
-      disabled: false,
-      name: 'aliquam tempus risus. Vestibulum hendrerit',
-      icon: 'right',
-      description: 'Donec vel magna et nulla porta laoreet. Pellentesque sed tortor ex. Ut luctus sodales ante, quis aliquam libero efficitur sit amet. In sodales pellentesque venenatis. Cras mattis suscipit diam, et aliquet metus vehicula non. Ut at rutrum nunc. Fusce lobortis, turpis ac interdum blandit, purus dui blandit justo',
-      customStyle: {
-        background: '#FCFCFC',
-        'border-radius': '4px',
-        'margin-bottom': '24px',
-        border: '0px',
-        color: 'white',
-        'margin-top': '-2em',
-        'font-size': '16px',
-      }
-    }
-  ];
 }
